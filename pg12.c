d0d992478002ccd0324fd4ce6bf24ddad95843ae
Advaith <169141726+Advaithacharya@users.noreply.github.com>
2025-05-21 22:36:09 +0530

    Update pg12.c

--- pg12.c
+++ pg12.c
@@ -1,96 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-#include<stdio.h> 
-#include<stdlib.h> #define MAX 50
+#define MAX 50
 
-int can_place(int c[],int r)
-
-{
-
-int i; for(i=0;i<r;i++)
-
-if(c[i]==c[r] || (abs(c[i]-c[r])==abs(i-r))) return 0;
-
-return 1;
-
+int can_place(int c[], int r) {
+    for (int i = 0; i < r; i++) {
+        if (c[i] == c[r] || abs(c[i] - c[r]) == abs(i - r))
+            return 0;
+    }
+    return 1;
 }
 
-void display(int c[],int n)
+void display(int c[], int n) {
+    char cb[10][10];
 
-{
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cb[i][j] = '-';
 
-int i,j;
+    for (int i = 0; i < n; i++)
+        cb[i][c[i]] = 'Q';
 
-char cb[10][10]; for(i=0;i<n;i++) for(j=0;j<n;j++) cb[i][j]='-';
-
-for(i=0;i<n;i++) cb[i][c[i]]='Q'; for(i=0;i<n;i++)
-
-{
-
-for(j=0;j<n;j++) printf("%c",cb[i][j]); printf("\n");
-
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            printf("%c ", cb[i][j]);
+        printf("\n");
+    }
+    printf("\n");
 }
 
+void n_queens(int n) {
+    int c[MAX];
+    int r = 0;
+
+    for (int i = 0; i < n; i++)
+        c[i] = -1;
+
+    while (r >= 0) {
+        c[r]++;
+        while (c[r] < n && !can_place(c, r))
+            c[r]++;
+
+        if (c[r] < n) {
+            if (r == n - 1) {
+                display(c, n);
+            } else {
+                r++;
+                c[r] = -1;
+            }
+        } else {
+            r--;  // Backtrack
+        }
+    }
 }
 
-void n_queens(int n)
+int main() {
+    int n;
 
-{
+    printf("Enter the number of queens: ");
+    if (scanf("%d", &n) != 1 || n <= 0 || n > 10) {
+        printf("Invalid input. Please enter a number between 1 and 10.\n");
+        return 1;
+    }
 
-int r;
+    printf("\nSolutions for %d-Queens problem:\n\n", n);
+    n_queens(n);
 
-int c[MAX]; c[0]=-1; r=0;
-
-{
-
- c[r]++;
-
- while(c[r]<n && !can_place(c,r))
-
-   c[r]++;
-
- if(c[r]<n)
-
- {
-
-  if(r==n-1)
-
-  {
-
-  display(c,n);
-
-  printf("\n\n");
-
- }
-
- else
-
-{
-
-  r++;
-
-  c[r]=-1;
-
- }
-
- }
-
- else
-
-   r--;
-
- }
-
+    return 0;
 }
-
- void main()
-
-{
-
-int n;
-
-printf("\nEnter the no. of queens:"); scanf("%d",&n);
-
-n_queens(n);
-
-}
-
