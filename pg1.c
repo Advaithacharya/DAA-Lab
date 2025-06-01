abf00c927c98fe819efb78a09ba92d9773638a67
Advaith <169141726+Advaithacharya@users.noreply.github.com>
2025-06-01 16:12:41 +0530

    Update pg1.c

--- pg1.c
+++ pg1.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<time.h>
 void main()
 {
@@ -6,7 +7,6 @@ int i,n,j,k,temp;
 printf("Enter the number of elements:\n");
 scanf("%d",&n);
 int a[n];
-printf("Enter  elements");
 srand(time(NULL));
 for(i=0;i<n;i++)
 {
